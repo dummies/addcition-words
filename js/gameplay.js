42b0e816cb7ede8bf7a446be61b125619b684479
dummies <saisreenivasan@live.in>
2012-12-25 19:12:10 +0530

    try

--- js/gameplay.js
+++ js/gameplay.js
@@ -7,36 +7,6 @@ var truth;
 var words;
 
 
-function ajaxFunction(){
-	var ajaxRequest;  // The variable that makes Ajax possible!
-	
-	try{
-		// Opera 8.0+, Firefox, Safari
-		ajaxRequest = new XMLHttpRequest();
-	} catch (e){
-		// Internet Explorer Browsers
-		try{
-			ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
-			try{
-				ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e){
-				// Something went wrong
-				alert("Your browser broke!");
-				return false;
-			}
-		}
-	}
-	// Create a function that will receive data sent from the server
-	ajaxRequest.onreadystatechange = function(){
-		if(ajaxRequest.readyState == 4 && ajaxRequest.status ==200){
-			truth = ajaxRequest.responseText;
-			console.log(truth);
-		}
-	}
-	ajaxRequest.open("GET", "js/phplogic.php?word="+str, true);
-	ajaxRequest.send(null); 
-}
 
 function loadwords()
 {
